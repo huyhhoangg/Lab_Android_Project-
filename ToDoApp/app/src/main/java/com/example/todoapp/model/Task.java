6ca00f22bb87e6eca2cbb68653cc5e7d07bbb489
huyhhoangg <huyhhoangg@gmail.com>
2025-08-05 13:52:10 +0700

    delete test

--- ToDoApp/app/src/main/java/com/example/todoapp/model/Task.java
+++
@@ -1,8 +0,0 @@
-package com.example.todoapp.model;
-
-public class Task {
-    private String name;
-    public Task(String name) { this.name = name; }
-    public String getName() { return name; }
-
-}
